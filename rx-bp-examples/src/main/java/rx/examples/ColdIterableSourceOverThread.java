128e596bff68ed3abefecbb9bceef103a696fc60
Ben Christensen <benjchristensen@netflix.com>
2014-03-03 15:26:49 -0800

    unit test placeholders

--- rx-bp-examples/src/main/java/rx/examples/ColdIterableSourceOverThread.java
+++ rx-bp-examples/src/main/java/rx/examples/ColdIterableSourceOverThread.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import rx.Observable;
 import rx.schedulers.Schedulers;
 
-public class ObservableFromIterable {
+public class ColdIterableSourceOverThread {
 
     public static void main(String[] args) {
 
